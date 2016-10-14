86f5abbc03880f990e3f98fc9444b39038d0f009
Boss <Boss@Boss-PC.fritz.box>
2016-10-14 18:38:15 +0200

    erstes kleines Menu hinzugefügt

--- Algo/Assignment1/view/MyFrame.java
+++ Algo/Assignment1/view/MyFrame.java
@@ -5,20 +5,34 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-@SuppressWarnings("serial")
+
 public class MyFrame extends JFrame implements ActionListener{
 	
 	public MyFrame(){
 		super("");
 		setLayout(new BorderLayout());
 		
+		JPanel pa = new JPanel();
+		pa.setLayout(new FlowLayout());
+		
+		JMenuBar mb = new JMenuBar();
+		JMenu m = new JMenu("Datei");
+		JMenu subm = new JMenu("Bild");
+		JMenuItem hin = new JMenuItem("Hinzufügen");
+		setJMenuBar(mb);
+		
+		mb.add(m);
+		m.add(subm);
+		subm.add(hin);
 		
 		addWindowListener(new WindowAdapter(){
             public void windowClosing(WindowEvent e){
                 System.exit(0);
             }
         });
-
+		
+		pack();
+		setVisible(true);
 	}
 	
 	
