02a822c34200394c028f401a6ded4018c7bec1fe
Julia <Julia@195.37.110.212>
2016-10-17 15:04:51 +0200

    m_ImgBig

--- Algo/Assignment1/model/Pic.java
+++ Algo/Assignment1/model/Pic.java
@@ -8,14 +8,16 @@ import javax.swing.JComponent;
 @SuppressWarnings("serial")
 public class Pic extends JComponent{
 	public Image m_Img;
+	public Image m_ImgBig;
 	int[] m_Pixel;
 	
 	Pic(File file){
-		m_Img = getToolkit().getImage(file.getAbsolutePath()).getScaledInstance(125,75, Image.SCALE_SMOOTH);
+		m_ImgBig = getToolkit().getImage(file.getAbsolutePath()).getScaledInstance(800,600, Image.SCALE_SMOOTH);
+		m_Img = m_ImgBig.getScaledInstance(125, 75, Image.SCALE_SMOOTH);
 	}
 	
 	public void paint(Graphics g){
-		g.drawImage(m_Img, 0, 0, getWidth(), getHeight(), this);
+		g.drawImage(m_Img, 0, 0, 125, 75, this);
 	}
 	
 	public Dimension getPreferredSize(){
