5de8218cd3d1f25bf90eb5da903191ae3e9594ef
Julia <Julia@172.20.112.25>
2016-12-08 08:34:26 +0100

    commit try

--- Assignment2/src/News.java
+++ Assignment2/src/News.java
@@ -1,4 +1,26 @@
 
 public class News {
-
+	String m_title;
+	String m_description;
+	
+	News(){
+		
+	}
+	
+	public void setTitle(String title) {
+		m_title = title;
+	}
+	
+	public void setDescription(String descpt) {
+		m_description = descpt;
+	}
+	
+	public String getTitle() {
+		return m_title;
+	}
+	
+	public String getDescription() {
+		return m_description;
+	}
+	
 }
